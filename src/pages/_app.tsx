2c6066d26c0a13ecfdcbf209231661ce853f2010
gabriels5g <gabrielspxls@gmail.com>
2023-04-21 01:15:56 -0300

    global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalStyles } from '@/styles/global'
+import "./styles.css"
 
+
+globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
+  
   return <Component {...pageProps} />
 }
