474da8c99b2b8a8f573501f1b398fabb901b16d8
gabriels5g <gabrielspxls@gmail.com>
2023-04-25 00:36:00 -0300

    formatting price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { useKeenSlider } from "keen-slider/react"
 import 'keen-slider/keen-slider.min.css'
 import Image from "next/image"
 import { stripe } from "@/lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
 
 
@@ -45,7 +45,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
@@ -57,7 +57,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: "BRL",
+      }).format(price.unit_amount / 100)
     }
    
     
@@ -65,6 +68,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 //2 hours
   }
 }
