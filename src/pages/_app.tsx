d3097ef430f55e28b4601260234869f377a4fb69
gabriels5g <gabrielspxls@gmail.com>
2023-05-02 00:58:44 -0300

    Unsuccessful purchase details

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,17 @@
-import type { AppProps } from 'next/app'
-import { globalStyles } from '@/styles/global'
-import logoImg from "../assets/logo.svg"
-import "./styles.css"
-import Image from 'next/image'
-import { Container, Header } from '@/styles/pages/app'
+import type { AppProps } from "next/app";
+import { globalStyles } from "@/styles/global";
+import logoImg from "../assets/logo.svg";
+import Image from "next/image";
+import { Container, Header } from "@/styles/pages/app";
 
-globalStyles()
+globalStyles();
 export default function App({ Component, pageProps }: AppProps) {
-  return( 
-  <Container>
-    <Header>
-      <Image  src={logoImg} alt='logo'/>
-    </Header>
-     <Component {...pageProps} />
-  </Container>
-  )
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import { HomeContainer, Product } from "@/styles/pages/home";
-import camiseta1 from "../assets/camisetas/1.png";
-import camiseta2 from "../assets/camisetas/2.png";
-import camiseta3 from "../assets/camisetas/3.png";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import Image from "next/image";
